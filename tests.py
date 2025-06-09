537de4c2fe90c1cff024aae7e2d37a78e1d51f55
Harry <61833884+hdavidson42@users.noreply.github.com>
2025-06-09 12:45:03 +0100

    added function to run python code

--- tests.py
+++ tests.py
@@ -1,10 +1,19 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
-tests = [
-    write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
-    write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
-    write_file("calculator", "/tmp/temp.txt", "this should not be allowed"),
-]
 
-for test in tests:
-    print(test)
+def test():
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
